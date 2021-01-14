26339b708873e0e608f56b9556bf08ba44c3c996
albert <albertmzehakov@gmail.com>
2021-01-14 16:28:56 +0300

    Почти все переделал + сделал проверку можно ли идти. В следующем комите займусь прыжками и гравитацией

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import pygame
-import os
-import sys
+import threading
 import time
 
-DISPLAY_SIZE = (1280, 720)
+DISPLAY_SIZE = (1024, 512)
 pygame.init()
-pygame.display.set_caption('Mario and Doom')
 screen = pygame.display.set_mode(DISPLAY_SIZE)
 start_game = True
 player_sprite = pygame.sprite.Group()
 world_sprite = pygame.sprite.Group()
 
-
-# def map_generator()
 
 def load_image(image_path, colorkey=None):
     result = pygame.image.load(image_path)
@@ -28,89 +24,94 @@ def load_image(image_path, colorkey=None):
 class Player(pygame.sprite.Sprite):
     image = load_image("player.png", -1)
 
-    def __init__(self, group, minX, maxX):
+    def __init__(self, group):
         super().__init__(group)
         self.image = Player.image
         self.rect = self.image.get_rect()
-        self.velocity_walk = 20
+        self.velocity_walk = 16
         self.velocity_jump = 50
-        # self.minX = minX
-        # self.maxX = maxX
-        self.rect.x = 50
-        self.rect.y = 300
-        self.ammo_1 = 12  # Дробь
-        self.ammo_2 = 60  # Пуля
-        self.ammo_3 = 150  # Энергия
-        self.ammo_4 = 3  # Топливо
-        self.ammo_5 = 3  # Снаряды для BFG
-        self.ammo_6 = 3  # Заряды для горнила
-        self.gun = None
-        self.isJumpXY = None
-        self.isJump = True
-        self.isJumpUp_old = True
-        self.jump_time = None
+        self.map_pos = [1, 0]
 
-    def next(self, dt):
-        self.rect.x = self.rect.x + self.velocity_walk * dt / 60
+        self.rect.x = 16
+        self.rect.y = 390
+
+    def next(self):
+        self.rect.x = self.rect.x + self.velocity_walk
         return self.rect.x
 
-    def back(self, dt):
-        self.rect.x = self.rect.x - self.velocity_walk * dt / 60
+    def back(self):
+        self.rect.x = self.rect.x - self.velocity_walk
         return self.rect.x
-
-    def jump(self, dt):
-        if self.jump_time is None or self.jump_time + 3 <= time.time():
-            if self.isJump:
-                self.isJumpXY = (self.rect.x, self.rect.y)
-                self.isJump = False
-                self.isJumpUp_old = True
-
-            if self.isJumpXY[1] - self.velocity_jump < self.rect.y and self.isJumpUp_old:
-                self.rect.y = self.rect.y - self.velocity_jump * dt / 60
-
-            elif self.isJumpXY[1] + self.velocity_jump > self.rect.y:
-                self.rect.y = self.rect.y + self.velocity_jump * dt / 60
-                self.isJumpUp_old = False
-            else:
-                self.isJump = True
-                self.jump_time = time.time()
-
-    def shoot(self):
-        if self.gun.isShoot:
-            pass
 
     def update(self):
         self.rect = self.rect
 
 
-player = Player(player_sprite, 0, 3000)
-clock = pygame.time.Clock()
+player = Player(player_sprite)
 
+max_map_pos = [61, 27]
+lvl_board = None
+
+
+def canGo(pos):
+    count = 0
+    for i in range(2):
+        for j in range(4):
+            if lvl_board[pos[1] + j][pos[0] + i] == '/':
+                count += 1
+    if count == 8:
+        return True
+    return False
+
+
+def map_generator(lvl):
+    global lvl_board
+    with open(lvl, mode='r', encoding='utf8') as f:
+        f = f.read()
+        lvl_board = f.replace(' ', '/').split('\n')
+        lvl_board.reverse()
+        for line in enumerate(f.split('\n')):
+            if len(line[1]) != 0:
+                for i in range(len(line[1])):
+                    if line[1][i] == '0':
+                        block = pygame.sprite.Sprite(world_sprite)
+                        block.image = load_image('block.png')
+                        block.rect = block.image.get_rect()
+                        block.rect.x = 16 * i
+                        block.rect.y = int(line[0]) * 16
+
+    grass = pygame.sprite.Sprite(world_sprite)
+    grass.image = load_image('grass-ground.png')
+    grass.rect = grass.image.get_rect()
+    grass.rect.x = 0
+    grass.rect.y = 448
+
+
+map_generator('lvl1.txt')
+
+clock = pygame.time.Clock()
 while start_game:
-    dt = clock.tick(60)
+
+    dt = clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start_game = False
 
     screen.fill((93, 148, 251))
 
-    keys = pygame.key.get_pressed()
-
     player_sprite.draw(screen)
-    if keys[pygame.K_d]:
-        player.next(dt)
-    if keys[pygame.K_a]:
-        player.back(dt)
-    if keys[pygame.K_SPACE]:
-        player.jump(dt)
-    #
-    # for x in range(col + 1):
-    #     pygame.draw.line(screen, (255, 255, 255), (margin + height * x, margin),
-    #                      (margin + height * x, DISPLAY_SIZE[1] - margin), width=1)
-    #
-    # for y in range(row + 1):
-    #     pygame.draw.line(screen, (255, 255, 255), (margin, margin + weight * y),
-    #                      (DISPLAY_SIZE[0] - margin, margin + weight * y), width=1)
-    # pygame.draw.rect(screen, pygame)
+    world_sprite.draw(screen)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d] and player.map_pos[0] + 1 <= max_map_pos[0]:
+        if canGo([player.map_pos[0] + 1, player.map_pos[1]]):
+            player.map_pos[0] += 1
+            player.next()
+
+    if keys[pygame.K_a] and player.map_pos[0] - 1 >= 1:
+        if canGo([player.map_pos[0] - 1, player.map_pos[1]]):
+            player.map_pos[0] -= 1
+            player.back()
+
     pygame.display.flip()
 pygame.quit()
